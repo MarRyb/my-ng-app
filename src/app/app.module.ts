12eeab90635aa303d35bb8cda9f9d9b88b90c0ca
MarGer <marinagernyak@gmail.com>
2019-01-07 03:02:45 +0200

    added service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
-import { ChildComponent }   from './child.component';
-
+  
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ChildComponent ],
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/phone.ts
@@ -0,0 +1,3 @@
+export class Phone {  
+  constructor(public name: string, public price: number) { }
+}
