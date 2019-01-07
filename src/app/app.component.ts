88abadfa8e96dff286529db619d1e201261f7e97
MarGer <marinagernyak@gmail.com>
2019-01-07 03:31:47 +0200

    added service data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,39 +6,8 @@ import {Phone} from './phone';
 @Component({
   selector: 'purchase-app',
   styleUrls: ['./app.component.css'],
-  template: `<div class="panel content-box">
-    <div class="form-inline">
-      <div class="form-group">
-        <div class="col-md-8">
-          <input class="form-control" [(ngModel)]="name" placeholder = "Модель" />
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-md-6">
-          <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена" />
-        </div>
-      </div>
-      <div class="form-group">
-        <div class="col-md-offset-2 col-md-8">
-          <button class="btn btn-default" (click)="addItem(name, price)">Добавить</button>
-        </div>
-      </div>
-    </div>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>Модель</th>
-          <th>Цена</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let item of items">
-          <td>{{item.name}}</td>
-          <td>{{item.price}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>`,
+  template: `<data-comp></data-comp>
+             <data-comp></data-comp>`,
   providers: [DataService, LogService]
 })
 export class AppComponent implements OnInit { 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { DataComponent }   from './data.component';
   
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent],
+  declarations: [ AppComponent, DataComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
